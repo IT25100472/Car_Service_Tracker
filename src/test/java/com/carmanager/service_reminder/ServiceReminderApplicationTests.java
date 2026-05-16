a08b35fcf02df2c400ec0e60863896f4b14c31b5
Janithka-IT25101649 <IT25101649@my.sliit.lk>
2026-05-16 20:25:57 +0530

    Initial Commit

---
+++ src/test/java/com/carmanager/service_reminder/ServiceReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carmanager.service_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
